80c1a81431dc5f009e3aa6ab45e25e90c4b29fae
Nasre Alam <nasrealam407@gmail.com>
2025-09-26 10:42:43 +0530

    Added: created image-editor page and pdf editor page.

---
+++ src/app/components/pdf-editor/pdf-editor.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf-editor',
+  templateUrl: './pdf-editor.component.html',
+  styleUrls: ['./pdf-editor.component.scss'],
+})
+export class PdfEditorComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
