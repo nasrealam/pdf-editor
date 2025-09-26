f6ec54efa138c259ebb4f774e86b11752ab0592d
Nasre Alam <nasrealam407@gmail.com>
2025-09-26 11:03:37 +0530

    U

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,23 +3,10 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-<<<<<<< HEAD
     loadComponent: () => import('./components/home/home.page').then((m) => m.HomePage),
-=======
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
-  {
-    path: 'pdf-editor',
-    loadComponent: () => import('./features/pdf-editor/components/pdf-editor.component')
-      .then(m => m.PdfEditorComponent)
-  },
-  {
-    path: 'image-editor',
-    loadComponent: () => import('./features/image-editor/components/image-editor.component')
-      .then(m => m.ImageEditorComponent)
->>>>>>> origin/main
-  },
-  {
+ 
+   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
--- src/app/components/home/home.page.ts
+++ src/app/components/home/home.page.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent,
+  IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+  IonButton
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    RouterModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonButton
+  ],
 })
 export class HomePage {
   constructor() {}
